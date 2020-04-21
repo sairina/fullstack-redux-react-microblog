ad2be60e3290462bc38af1e0fd00743a0117d891
sairina <sairina.merinotsui@gmail.com>
2020-04-21 11:29:51 -0700

    moved BrowserRouter to index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Routes from './components/Routes';
@@ -7,12 +6,10 @@ import Routes from './components/Routes';
 function App() {
   
   return (
-    <BrowserRouter>
       <div className="App">
         <Navbar />
         <Routes />
       </div>
-    </BrowserRouter>
   );
 }
 
