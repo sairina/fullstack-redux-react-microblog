1ff726ce4afae54c47f28e1aff2820ec903570b3
sairina <sairina.merinotsui@gmail.com>
2020-04-20 20:34:30 -0700

    added functions cance and save as props

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
+// import { useDispatch } from "react-redux";
 import PostForm from './PostForm';
 
 function NewPost() {
+  // const dispatch = useDispatch();
+  const history = useHistory();
+
+  // Adds a new post and redirects to homepage
+  function add({ title, description, body}) {
+    // dispatchEvent(sendPostToAPI(title, description, body));
+    history.push('/');
+  }
+
+  // Cancels and redirects to homepage
+  function handleCancel() {
+    history.push('/');
+  }
   return (
     <>
       <h1>New Post</h1>
-      <PostForm />
+      <PostForm cancel={handleCancel} save={add}/>
     </>
   );
 }
